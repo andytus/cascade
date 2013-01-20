d602c53d981c902b90c539e88efb27f4f1431ac9
joebeone <jbennett@webrapids.com>
2013-01-20 17:03:25 -0500

    Still configuring for heroku

--- cascade/settings.py
+++ cascade/settings.py
@@ -115,7 +115,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'cascade.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'cascade.wsgi.application'
