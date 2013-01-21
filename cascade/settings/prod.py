85a42ecc139e54df145c055fb7569c9b8b2512aa
joebeone <jbennett@webrapids.com>
2013-01-20 19:45:16 -0500

    Still configuring for heroku

--- cascade/settings/prod.py
+++ cascade/settings/prod.py
@@ -1,5 +1,18 @@
 from common import *
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+from urlparse import urlparse
+#import dj_database_url
+#DATABASES['default'] =  dj_database_url.config()
+
+
+if os.environ.has_key('DATABASE_URL'):
+    url = urlparse(os.environ['DATABASE_URL'])
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD': url.password,
+        'HOST': url.hostname,
+        'PORT': url.port,
+        }
