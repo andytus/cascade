c239e25fb5e6d0ebaa27565a6ebb710dbe0ff0f9
joebeone <jbennett@webrapids.com>
2013-10-21 23:01:47 -0400

    created a generic load_defaults script for loading default values into CartLogic cartmanager app

---
+++ cascade/libs/load_defaults.py
@@ -0,0 +1,68 @@
+__author__ = 'jbennett'
+
+from cascade.apps.cartmanager.models import CartStatus, CartType, TicketStatus, CartServiceType
+
+
+class LoadData:
+
+    def __init__(self):
+        self.default_site = 2
+        self.cart_types = [{'name': 'Trash', 'size': 96},
+                           {'name': 'Trash', 'size': 64},
+                           {'name': 'Trash', 'size': 35},
+                           {'name': 'Recycle', 'size': 96},
+                           {'name': 'Recycle', 'size': 64},
+                           {'name': 'Recycle', 'size': 35}
+                           ]
+        self.service_types = [{'service': 'Exchange Remove', 'code': 'EX-REM', 'description': 'Exchange Remove',
+                                'complete_cart_status_change':'Inventory'},
+                                {'service': 'Exchange Delivery', 'code': 'EX-DEL', 'description': 'Exchange Remove',
+                                'complete_cart_status_change':'Delivered'},
+                                {'service': 'Delivery', 'code': 'DEL', 'description': 'Delivery',
+                                'complete_cart_status_change':'Delivered'},
+                                {'service': 'Remove', 'code': 'REM', 'description': 'Remove',
+                                'complete_cart_status_change': 'Inventory'},
+                              ]
+
+        self.cart_status = [{'level': 'Default', 'label': 'Repaired'},
+                            {'level': 'Info', 'label': 'Inventory'},
+                            {'level': 'Damaged', 'label': 'Damaged'},
+                            {'level': 'Warning', 'label': 'Lost'},
+                            {'level': 'Success', 'label': 'Delivered'}
+                            ]
+        self.ticket_status = [{'level': 'Info', 'service_status': 'Requested'},
+                              {'level': 'Completed', 'service_status': 'Success'},
+                              {'level': 'Alert', 'service_status': 'Unsuccessful'},
+                              {'level': 'Info', 'service_status': 'Uploaded'},
+                              ]
+
+    def load_cart_types(self):
+        for record in self.cart_types:
+            cart_type = CartType(site=self.default_site, name=record['name'], size=record['size'])
+            cart_type.save()
+
+    def load_cart_status(self):
+        for record in self.cart_status:
+            cart_status = CartStatus(site=self.default_site, level=record['level'], label=record['label'])
+            cart_status.save()
+
+    def load_service_types(self):
+        for record in self.service_types:
+            service_type = CartServiceType(site=self.default_site, service=record['service'],
+                                           code=record['code'], description=record['description'],
+                                           complete_cart_status_change=
+                                           CartStatus.objects.get(label=record['complete_cart_status_change'])
+                                           )
+            service_type.save()
+
+    def load_ticket_status(self):
+        for record in self.ticket_status:
+            ticket_status = TicketStatus(site=self.default_site, level=record['level'],
+                                         service_status=record['service_status'])
+            ticket_status.save()
+
+
+
+
+
+
