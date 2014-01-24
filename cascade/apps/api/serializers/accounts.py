89df6636cd1930d232b780c36acb8d074213a682
joebeone <jbennett@webrapids.com>
2014-01-24 18:21:18 -0500

    Added accounts serializer for profile api

---
+++ cascade/apps/api/serializers/accounts.py
@@ -0,0 +1,15 @@
+__author__ = 'jbennett'
+from rest_framework import serializers
+from cascade.apps.accounts.models import Profile
+from django.contrib.sites.models import Site
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    sites = serializers.SlugRelatedField(source='sites', read_only=True, many=True, slug_field='domain')
+    username = serializers.SlugRelatedField(source='user', read_only=True, slug_field='username')
+    class Meta:
+        model = Profile
+        fields = ('user', 'sites', 'company', 'mugshot')
+
+
+
