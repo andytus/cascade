c45d8cf952afa485ce08022574462aaddfce3e20
joebeone <jbennett@webrapids.com>
2014-02-13 18:08:23 -0500

    Updated TicketsReportViewModel comment

--- cascade/settings/prod.py
+++ cascade/settings/prod.py
@@ -19,8 +19,8 @@ if os.environ.has_key('DATABASE_URL'):
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'cascade.libs.storage.S3PipelineStorage'
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ""
+AWS_ACCESS_KEY_ID = os.environ['AWS_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_BUCKET']  #adding storage location from environmental variable
 AWS_PRELOAD_METADATA = True
 AWS_QUERYSTRING_AUTH = False
